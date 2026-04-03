2eb0427726474c4e702fcfb652c058bd667b8e40
yashnaiduu <yash.22bce8038@vitapstudent.ac.in>
2026-04-03 22:30:21 +0530

    network

--- src/main.ts
+++ src/main.ts
@@ -37,6 +37,8 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on port ${port}`);
 }
 void bootstrap();
